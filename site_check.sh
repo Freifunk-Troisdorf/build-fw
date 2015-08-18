853bd37e3a0306b38d303fac2428ef0ff71d4cb6
stebifan <stefan@tro-mail.de>
2015-08-18 16:27:05 +0200

    Update site_check.sh

--- site_check.sh
+++ site_check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /home/travis/build/Freifunk-Troisdorf/gluon/
 make update
-make -j4 GLUON_BRANCH=testing GLUON_TARGET=ar71xx-generic DEFAULT_GLUON_RELEASE=testing
+make -j5 GLUON_BRANCH=testing GLUON_TARGET=ar71xx-generic DEFAULT_GLUON_RELEASE=testing
